9ce45f69a164a4747a5d0aae1ff71ad6d7e0968d
Arda Şalvarlılar <ardasalvarlilar@192.168.1.6>
2023-09-21 08:12:16 +0300

    added new  if else statements

--- script.js
+++ script.js
@@ -13,11 +13,23 @@ document.querySelector('.check').addEventListener('click', function(){
   }else if(guess === secretNumber){
     document.querySelector('.message').textContent ='🎉 Correct Number'
   }else if(guess > secretNumber){
-    document.querySelector('.message').textContent = '📈 Too high!'
-    score--
-    document.querySelector('.score').textContent =score
+    if(score > 1){
+      document.querySelector('.message').textContent = '📈 Too high!'
+      score--
+      document.querySelector('.score').textContent = score
+    }else{
+      document.querySelector('.message').textContent = '💥 You lost the game!'
+      document.querySelector('.score').textContent = 0
+    }
   }else if(guess < secretNumber){
-    document.querySelector('.message').textContent = '📉 Too low!'
-    document.querySelector('.score').textContent =score
+
+    if(score > 1){
+      document.querySelector('.message').textContent = '📈 Too low!'
+      score--
+      document.querySelector('.score').textContent = score
+    }else{
+      document.querySelector('.message').textContent = '💥 You lost the game!'
+      document.querySelector('.score').textContent = 0
+    }
   }
 })
