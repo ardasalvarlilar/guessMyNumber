799da9b852fe90bfa1c20de0740092b795d0b5f2
Arda Şalvarlılar <ardasalvarlilar@192.168.1.6>
2023-09-21 08:40:06 +0300

    added style features and debugging

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 'use strict';
 
-const secretNumber = Math.floor(Math.random() * 20)
+let secretNumber = Math.floor(Math.random() * 20)
 let score = 20
 document.querySelector('.number').textContent = secretNumber
+
+document.querySelector('.again').addEventListener('click', () => {
+  secretNumber = Math.floor(Math.random() * 20)
+  document.querySelector('.number').textContent = secretNumber
+  document.querySelector('body').style.backgroundColor = '#222'
+  document.querySelector('.guess').value = ''
+});
 
 document.querySelector('.check').addEventListener('click', function(){
   const guess = Number(document.querySelector('.guess').value)
   console.log(guess)
 
+  // when no input
   if(!guess){
     document.querySelector('.message').textContent = '⛔️ No Number!'
+
+    // when player wins
   }else if(guess === secretNumber){
     document.querySelector('.message').textContent ='🎉 Correct Number'
+    document.querySelector('body').style.backgroundColor = '#60b347'
+    document.querySelector('.number').style.width = '30rem'
+
+    // when guess is too high
   }else if(guess > secretNumber){
     if(score > 1){
       document.querySelector('.message').textContent = '📈 Too high!'
@@ -21,6 +35,8 @@ document.querySelector('.check').addEventListener('click', function(){
       document.querySelector('.message').textContent = '💥 You lost the game!'
       document.querySelector('.score').textContent = 0
     }
+
+    // when guess is too low
   }else if(guess < secretNumber){
 
     if(score > 1){
